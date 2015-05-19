9fe6d2e5a26a87d88cbf66c24c482556aef0bda6
Andreas Litt <andreas.litt@gmail.com>
2015-05-19 11:46:05 +0200

    enforce https not only while building container also while running

--- prepare_dokku.sh
+++ prepare_dokku.sh
@@ -3,6 +3,9 @@ cat /config/nodename > $DOKKU_ROOT/HOSTNAME
 cat /config/nodename > $DOKKU_ROOT/VHOST
 [[ -e $DOKKU_ROOT/.ssh/authorized_keys ]] || touch $DOKKU_ROOT/.ssh/authorized_keys
 [[ -e $DOKKU_ROOT/.sshcommand ]] || /usr/local/bin/sshcommand create dokku `which dokku`
+mkdir -p $DOKKU_ROOT/tls
+[[ -e $DOKKU_ROOT/tls/server.crt ]] || touch $DOKKU_ROOT/tls/server.crt
+[[ -e $DOKKU_ROOT/tls/server.key ]] || touch $DOKKU_ROOT/tls/server.key
 
 # rebuild all nginx.conf (only needed if hostname was changed)
 # Strange but we need 'dokku domains:clear' here o.O
