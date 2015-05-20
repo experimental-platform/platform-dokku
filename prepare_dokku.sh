8ae75777271c10a4787b2b42cae942948734d6ef
Andreas Litt <andreas.litt@gmail.com>
2015-05-20 13:50:20 +0200

    generate ssh configuration while starting dokku

--- prepare_dokku.sh
+++ prepare_dokku.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 echo $HOST_HOSTNAME > $DOKKU_ROOT/HOSTNAME
 cat /config/nodename > $DOKKU_ROOT/VHOST
-[[ -e $DOKKU_ROOT/.ssh/authorized_keys ]] || touch $DOKKU_ROOT/.ssh/authorized_keys
 [[ -e $DOKKU_ROOT/.sshcommand ]] || /usr/local/bin/sshcommand create dokku `which dokku`
 
+echo "" > $DOKKU_ROOT/.ssh/authorized_keys
+for KEYFILE in /config/ssh/*
+do
+  if [[ -f $KEYFILE ]]; then
+    FINGERPRINT=$(ssh-keygen -lf $KEYFILE | cut -d \  -f 2)
+    KEY=$(cat $KEYFILE)
+    echo "command=\"FINGERPRINT=$FINGERPRINT NAME=$KEYFILE `cat $DOKKU_ROOT/.sshcommand` $SSH_ORIGINAL_COMMAND\",no-agent-forwarding,no-user-rc,no-X11-forwarding,no-port-forwarding $KEY" >> $DOKKU_ROOT/.ssh/authorized_keys
+  fi
+done
 # rebuild all nginx.conf (only needed if hostname was changed)
 # Strange but we need 'dokku domains:clear' here o.O
 dokku apps | tail -n +2 | xargs -n1 dokku domains:clear
